fa12069ce4da89e296fa45ce42097d242f6d4535
Raksus <irda22r@gmail.com>
2016-09-14 15:37:10 +0200

    getGuildProfile

--- connection.py
+++ connection.py
@@ -1,8 +1,31 @@
 import configparser
+import requests
 
-config = configparser.ConfigParser()
-config.read('api.cfg')
+class Connection(object):
 
-API_KEY = config['DEFAULT']['api_key']
 
-print(API_KEY)
+	def __init__(self):
+		config = configparser.ConfigParser()
+		config.read('api.cfg')
+
+		self.API_KEY = config['DEFAULT']['api_key']
+		self.LOCALE = config['DEFAULT']['locale']
+		self.WOW = "wow"
+		self.REGION = config['DEFAULT']['region']
+
+	def requestJson(self, url):
+		r = requests.get(url)
+		return r.json()
+
+	# c.getGuildProfile(name="Resort", realm="C'thun", fields=["members", "achievements"])
+	def getGuildProfile(self, name, realm, fields=None):
+		name = name.lower().replace (" ", "%2C")
+		realm = realm.lower().replace (" ", "%2C")
+		if fields:
+			fields = "%2C".join(fields)
+
+		url = "https://%s.api.battle.net/%s/guild/%s/%s?fields=%s&locale=%s&apikey=%s" % (self.REGION, self.WOW, realm, name, fields, self.LOCALE, self.API_KEY)
+		return self.requestJson(url)
+
+c = Connection()
+c.getGuildProfile(name="Resort", realm="C'thun", fields=["members"])
