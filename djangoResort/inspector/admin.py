e7d4da1aac442153df787966c2956887b5458f3f
Raksus <irda22r@gmail.com>
2016-09-20 09:02:56 +0000

    Refactor Models

--- djangoResort/inspector/admin.py
+++ djangoResort/inspector/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Players
+from .models import Player, Item
 
 # Register your models here.
 
-admin.site.register(Players)
+admin.site.register(Player)
+admin.site.register(Item)
